61687011e3bdea664142c19e5768d093d02c512b
Gavin Miller <gavin@petrofeed.com>
2013-10-01 09:26:14 -0600

    Add rspec and main code

---
+++ lib/ensure_param_exists.rb
@@ -0,0 +1,11 @@
+module EnsureParamExists
+  def define_ensure_param_exists_for(*expected_params)
+    expected_params.each do |expected_param|
+      method_name = "ensure_#{expected_param}_exists"
+      define_method(method_name) do
+        return unless params[expected_param.to_sym].blank?
+        render json: { success: false, message: "missing #{expected_param.to_s} parameter" }, status: 422
+      end
+    end
+  end
+end
---
+++ spec/ensure_param_exists_spec.rb
@@ -0,0 +1,24 @@
+require 'ensure_param_exists'
+
+class TestClass
+  extend EnsureParamExists
+end
+
+describe EnsureParamExists do
+  it "accepts symbols" do
+    class TestClass
+      define_ensure_param_exists_for :test
+    end
+
+    TestClass.method_defined?(:ensure_test_exists).should be_true
+  end
+
+  it "accepts arrays" do
+    class TestClass
+      define_ensure_param_exists_for :test, :test2
+    end
+
+    TestClass.method_defined?(:ensure_test2_exists).should be_true
+  end
+end
+
