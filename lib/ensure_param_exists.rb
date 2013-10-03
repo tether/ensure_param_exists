36551a987298462a5855901915a51454d27e8dbf
Gavin Miller <gavin@petrofeed.com>
2013-10-03 15:56:25 -0600

    Refactored the logic into a single method

--- lib/ensure_param_exists.rb
+++ lib/ensure_param_exists.rb
@@ -6,37 +6,31 @@ module EnsureParamExists
 
   included do
     def self.ensure_param(expected_param, opts = {})
-      method_name = "ensure_#{expected_param}_exists"
-      define_method(method_name) do
-        return unless params[expected_param.to_sym].blank?
-        render json: { success: false, message: "missing #{expected_param.to_s} parameter" }, status: 422
-      end
-
-      before_filter method_name.to_sym, opts
+      _ensure_param_exists_(:and, [expected_param, opts])
     end
 
     def self.ensure_any_params(*expected_params)
+      _ensure_param_exists_(:or, expected_params)
+    end
+
+    def self.ensure_all_params(*expected_params)
+      _ensure_param_exists_(:and, expected_params)
+    end
+
+    def self._ensure_param_exists_(operator, expected_params)
       opts = expected_params.last.kind_of?(Hash) ? expected_params.pop : {}
 
-      method_name = "ensure_#{expected_params.join("_or_")}_exists"
+      method_name = "ensure_#{expected_params.join("_#{operator}_")}_exists"
       define_method(method_name) do
-        return if expected_params.any? { |expected_param| params[expected_param.to_sym].present? }
-        render json: { success: false, message: "missing #{expected_params.join(" or ")} parameter" }, status: 422
+        return if expected_params.send(_operator_map_[operator]) { |expected_param| params[expected_param.to_sym].present? }
+        render json: { success: false, message: "missing #{expected_params.join(" #{operator} ")} parameter" }, status: 422
       end
 
       before_filter method_name.to_sym, opts
     end
 
-    def self.ensure_all_params(*expected_params)
-      opts = expected_params.last.kind_of?(Hash) ? expected_params.pop : {}
-
-      method_name = "ensure_#{expected_params.join("_and_")}_exists"
-      define_method(method_name) do
-        return if expected_params.all? { |expected_param| params[expected_param.to_sym].present? }
-        render json: { success: false, message: "missing #{expected_params.join(" and ")} parameter" }, status: 422
-      end
-
-      before_filter method_name.to_sym, opts
+    def _operator_map_
+      { and: 'all?', or: 'any?' }
     end
   end
 end
